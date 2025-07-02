c717fd1122ab30f97827f55249491d588353bbfb
agussfw17 <agusfw17@gmail.com>
2025-07-02 17:53:14 -0300

    asd

--- js/script.js
+++ js/script.js
@@ -1,24 +1,31 @@
+const presupuesto = {
+  moneda: 0,
+  importe: 0,
+};
+
 const gastos = [];
 
 const monedas = [
-  { id: "1", sym: "$", cotizacion: 1 },
-  { id: "2", sym: "U$S", cotizacion: 41.2 },
+  { id: 1, sym: "$", cotizacion: 1 },
+  { id: 2, sym: "U$S", cotizacion: 41.2 },
 ];
 
-function soloNumero() {
-  this.value = this.value.replace(/\D/g, "");
-}
-
 function validarNumero(valor) {
-  return !isNaN(valor) && parseFloat(valor) >= 0;
+  return !isNaN(valor) && parseFloat(valor, 2) >= 0;
 }
 
-function pedirDetalle() {
-  return prompt("Ingrese un breve detalle de su gasto");
+function pedirImporte(msg) {
+  let numero = prompt(msg);
+  if (!validarNumero(numero)) {
+    do {
+      numero = prompt("Debe ingresar un numero valido");
+    } while (!validarNumero(numero));
+  }
+  return parseFloat(numero, 2);
 }
 
 function monedaValida(moneda) {
-  return monedas.some((m) => m.id === moneda);
+  return monedas.some((m) => m.id === parseInt(moneda));
 }
 
 function pedirMoneda() {
@@ -27,22 +34,36 @@ function pedirMoneda() {
     moneda = prompt(`Seleccione la moneda:
         1 - $
         2 - U$S`);
+    if (moneda === null) {
+      return null;
+    }
   } while (!monedaValida(moneda));
-  return monedas.find((m) => m.id === moneda);
+  return monedas.find((m) => m.id === parseInt(moneda)).id;
 }
 
-function pedirImporte() {
-  let importe = prompt("Ingresa el importe gastado");
-  if (!validarNumero(importe)) {
-    do {
-      importe = prompt("Debe ingresar un numero valido");
-    } while (!validarNumero(importe));
+function pedirPresupuesto() {
+  alert(
+    `Bienvenido al controlador de gastos. A continuación ingrese la moneda de su presupuesto inicial.`
+  );
+  let moneda = pedirMoneda();
+  if (moneda) {
+    let importe = pedirImporte(
+      "Por favor ingrese su presupuesto para empezar."
+    );
+    if (importe) {
+      presupuesto.importe = importe;
+      presupuesto.moneda = moneda;
+    }
   }
-  return parseFloat(importe);
 }
 
-function convertirAPesos(moneda, importe) {
-  let cotizacion = parseFloat(monedas.find((m) => m.id === moneda).cotizacion);
+function buscarMoneda(monedaId) {
+  return monedas.find((moneda) => moneda.id === monedaId);
+}
+
+function convertirAPesos(monedaId, importe) {
+  let moneda = buscarMoneda(monedaId);
+  let cotizacion = moneda.cotizacion;
   return importe * cotizacion;
 }
 
@@ -51,6 +72,7 @@ function calcularTotal() {
   gastos.forEach((gasto) => {
     total += convertirAPesos(gasto.moneda.id, gasto.importe);
   });
+  alert(`El total de los gastos es $ ${total}`);
   return parseFloat(total, 2);
 }
 
@@ -62,13 +84,12 @@ function imprimirGastos() {
   });
   let total = calcularTotal();
   listaGastos.innerHTML += `<tr><td>Total</td><td>$${total}</td></tr>`;
-  alert(`El total de los gastos es $ ${total}`);
 }
 
 function agregarGasto() {
   let detalle = prompt("Ingrese un breve detalle de su gasto");
   let moneda = pedirMoneda();
-  let importe = pedirImporte();
+  let importe = pedirImporte("Ingrese cuanto gasto");
 
   let gasto = {
     detalle: detalle,
@@ -77,16 +98,42 @@ function agregarGasto() {
   };
 
   gastos.push(gasto);
-  imprimirGastos();
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  const inputPresupuesto = document.getElementById("presupuesto");
-  inputPresupuesto.addEventListener("input", soloNumero);
+function pedirAccion() {
+  let accion = prompt(`Seleccione la acción que quiera:
+        1 - Agregar gasto
+        2 - Calcular presupuesto restante
+        3 - Calcular total gastado
+        4- Salir
+    `);
 
-  const inputGasto = document.getElementById("gasto");
-  inputGasto.addEventListener("input", soloNumero);
+  console.log("accion", accion);
+  switch (parseInt(accion)) {
+    case 1:
+      agregarGasto();
+      pedirAccion();
+      break;
 
-  const buttonAddGasto = document.getElementById("bgasto");
-  buttonAddGasto.addEventListener("click", agregarGasto);
-});
+    case 2:
+      break;
+
+    case 3:
+      calcularTotal();
+      pedirAccion();
+      break;
+
+    case 4:
+      return;
+
+    default:
+      pedirAccion();
+  }
+}
+
+function main() {
+  pedirPresupuesto();
+  pedirAccion();
+}
+
+main();
